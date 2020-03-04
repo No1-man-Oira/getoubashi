9c671fc7404e36bc06278c8f0e58f3cbf03efc15
kunisato <kunisato@intra-mart.jp>
2020-03-04 19:59:01 +0900

    update

--- js/parts/discography/discographySection.js
+++ js/parts/discography/discographySection.js
@@ -1,11 +1,12 @@
-const soundcloudURL = 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/971590474&color=%23130a1a&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true';
+const soundcloudTrackURL = 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/971590474';
+const soundcloudWidgetOption = '&color=%23bcb890&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true';
 
 document.write(`
   <section class="hero is-fullheight" id="discography">
     <div class="hero-body">
       <div class="container has-text-centered">
-        <iframe id="sound" width="100%" height="450" scrolling="no" frameborder="no"
-          src="${soundcloudURL}">
+        <iframe id="sound" width="100%" height="450" allow="autoplay" scrolling="no" frameborder="no"
+          src="${soundcloudTrackURL + soundcloudWidgetOption}">
         </iframe>
         <div class="volumebar" id="volumebar" style="display: none">
           <div>Volume</div>
